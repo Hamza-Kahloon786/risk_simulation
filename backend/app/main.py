6cbbec8ad41508e69b515cfbfaf4d038a3e6bd22
Hamza Kahloon <hamzaakahloon903@gmail.com>
2025-08-19 12:08:12 -0700

    updation

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,14 +15,14 @@ app = FastAPI(title="Risk Simulation API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000",
+        # "http://localhost:5173",
+        # "http://localhost:3000",
+        # "http://127.0.0.1:5173",
+        # "http://127.0.0.1:3000",
         "*"  # Allow all origins for development
     ],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
